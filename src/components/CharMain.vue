39936cb81c7eb29807a78ea0cdabb0408c5607f4
Øyvind Langeggen <oyvind@increo.no>
2024-02-26 09:40:34 +0100

    Multiple edits

--- src/components/CharMain.vue
+++ src/components/CharMain.vue
@@ -1,76 +1,142 @@
 <script setup lang="ts">
-import DATA from '../data/data.json'
+import DATA from '../data/data.json';
 import type { Character } from '../types/Character';
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 import CharItem from './CharItem.vue';
 
-const characters = DATA as Character[];
-const uniqueRaces = [...new Set(characters.map(item => item.race))];
-const filterText = ref();
-const filterRace = ref();
-// const filteredChars = computed(() => {
-//     console.log("computing");
-    
-//     let filter = filterText;
-// 	if (!filter.value) {
-//         return characters;
-//     }
-//     return characters.filter( char => 
-//         char.name.toLowerCase().includes(filter.value.toLowerCase())
-//     )
-// })
-const filteredChars = computed(() => {
-    console.log("computing");
-    
-    let filter = filterText;
-	if (!filter.value) {
-        return characters;
-    }
-    return characters.filter( char => 
-        char.name.toLowerCase().includes(filter.value.toLowerCase())
-)
+const characters = (DATA as Character[]).sort((a, b) => {
+    return -a.createdAt.localeCompare(b.createdAt)
 });
-const onRaceChange = () => {
-    console.log(filterRace.value);   
-};
+const uniqueRaces = [...new Set(characters.map((item) => item.race))];
+const uniqueClasses = [...new Set(characters.map((item) => item.className))];
+const filterRace = ref("");
+const filterClass = ref("");
+const searchInput = ref("");
+
+let filteredChars = ref(characters);
+let searchActive = ref(false);
+let sortOrder = ref(1);
+
+function submit () {
+    if (!searchInput.value) {
+        filteredChars.value = characters;
+    } else {
+        filteredChars.value = characters.filter((char) =>
+            char.name.toLowerCase().includes(searchInput.value.toLowerCase())
+        );
+    }
+
+    filter();
+    searchActive.value = true;
+}
+
+function filterChanged() {
+    if (searchActive.value && searchInput.value) {
+        console.log(`Searched for: `, searchInput.value);
+        filteredChars.value = characters.filter((char) =>
+            char.name.toLowerCase().includes(searchInput.value.toLowerCase())
+        );
+    } else {
+        filteredChars.value = characters;    
+    }
+
+    filter();
+}
+
+function filter() {
+    filteredChars.value = filteredChars.value.filter(function(char) {
+        let filtered = true;
+        if (filterRace.value && filterRace.value.length > 0) {
+          filtered = char.race == filterRace.value
+        }
+
+        if (filtered) {
+          if (filterClass.value && filterClass.value.length > 0) {
+            filtered = char.className == filterClass.value
+          }
+        }        
+
+        return filtered;
+    });
+}
+
+function clearSearch() {
+    searchInput.value = "";
+    searchActive.value = false;
+    filteredChars.value = characters;
+    filter();
+}
+
+function sortDate() {
+    filteredChars.value = filteredChars.value.sort((a,b) => {
+        if (sortOrder.value == 1) {
+            return a.createdAt.localeCompare(b.createdAt)
+        } else  {
+            return -a.createdAt.localeCompare(b.createdAt)
+        }
+    })
+    sortOrder.value = -sortOrder.value;
+}
 </script>
 
 <template>
     <div class="char-holder">
         <div class="char-inner">
-    <div class="search">
-      <input
-        class="search-box"
-		placeholder="Søk på navn..."
-		v-model="filterText"
-	/>
+            <form class="search-form" ref="form" v-on:submit.prevent="submit">
+                <div class="search-form__inner">
+                <label class="search-form__magnifier-label" for="search-input" id="search-label"
+                    ></label>
+                <input class="search-form__input" type="text" v-model="searchInput" aria-label="search term" />           
+                    <button type="reset" title="Nullstill søket" @click="clearSearch" :class="{ hidden: searchInput =='' }" class="search-form__reset clear" aria-label="Nullstill søket">
+                        <svg width="20" height="20" viewBox="0 0 20 20"><path d="M10 10l5.09-5.09L10 10l5.09 5.09L10 10zm0 0L4.91 4.91 10 10l-5.09 5.09L10 10z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    </button>
+                    <button class="search-form__magnifier" type="submit" aria-label="search">
+                        <svg width="20" height="20" class="search-form__label" viewBox="0 0 20 20">
+                        <path
+                            d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+                            stroke="currentColor"
+                            fill="none"
+                            fill-rule="evenodd"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        ></path></svg
+                >
+                    </button>
+            </div>
+            </form>
+            <div>{{ searchActive ? searchInput : "" }}</div>
+            <div class="filters">
+                <select v-model="filterRace" @change="filterChanged()">
+                    <option value="">Velg rase</option>
+                    <option v-for="(item, key) in uniqueRaces" :key="key" :value="item">
+                        {{ item }}
+                    </option>
+                </select>
+                <select v-model="filterClass" @change="filterChanged()">
+                    <option value="">Velg klasse</option>
+                    <option v-for="(item, key) in uniqueClasses" :key="key" :value="item">
+                        {{ item }}
+                    </option>
+                </select>                
+            </div>
+        </div>
+        <div class="list">
+            <table class="styled-table">
+                <thead>
+                    <tr>
+                        <th>Navn</th>
+                        <th>Rase</th>
+                        <th>Klasse</th>
+                        <th>Nivå</th>
+                        <th>Lagret <button @click="sortDate">Sort</button></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <CharItem v-for="(char, i) in filteredChars" :character="char" :key="i" />
+                </tbody>
+            </table>
+        </div>
     </div>
-    <div class="filters">
-        <select v-model="filterRace" @change="onRaceChange()">
-            <option selected value="">Velg rase</option>
-            <option v-for="(item, key) in uniqueRaces" :key="key" :value="item">
-                {{item}}
-            </option>
-        </select>
-    </div>
-</div>
-    <div class="list">
-        <table class="styled-table">
-            <thead>
-                <tr>
-                    <th>Navn</th>
-                    <th>Rase</th>
-                    <th>Klasse</th>
-                    <th>Nivå</th>
-                    <th>Lagret</th>
-                </tr>
-            </thead>
-            <tbody>
-                <CharItem v-for="(char, i) in filteredChars" :character="char" :key="i"/>
-        </tbody>
-        </table>
-    </div>
-</div>
 </template>
 
 <style scoped>

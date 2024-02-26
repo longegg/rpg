bedb697984c0dc451ce61b3d060289ca830f2b58
Øyvind Langeggen <oyverik@gmail.com>
2024-02-26 11:58:08 +0100

    Edit search form

--- src/components/CharMain.vue
+++ src/components/CharMain.vue
@@ -5,19 +5,20 @@ import { ref } from 'vue';
 import CharItem from './CharItem.vue';
 
 const characters = (DATA as Character[]).sort((a, b) => {
-    return -a.createdAt.localeCompare(b.createdAt)
+    return -a.createdAt.localeCompare(b.createdAt);
 });
 const uniqueRaces = [...new Set(characters.map((item) => item.race))];
 const uniqueClasses = [...new Set(characters.map((item) => item.className))];
-const filterRace = ref("");
-const filterClass = ref("");
-const searchInput = ref("");
+const filterRace = ref('');
+const filterClass = ref('');
+const searchInput = ref('');
 
 let filteredChars = ref(characters);
 let searchActive = ref(false);
+let filtersActive = ref(false);
 let sortOrder = ref(1);
 
-function submit () {
+function submit() {
     if (!searchInput.value) {
         filteredChars.value = characters;
     } else {
@@ -37,44 +38,53 @@ function filterChanged() {
             char.name.toLowerCase().includes(searchInput.value.toLowerCase())
         );
     } else {
-        filteredChars.value = characters;    
+        filteredChars.value = characters;
     }
 
     filter();
+
+    filtersActive.value = (filterRace.value || filterClass.value) ? true : false;
 }
 
 function filter() {
-    filteredChars.value = filteredChars.value.filter(function(char) {
+    filteredChars.value = filteredChars.value.filter(function (char) {
         let filtered = true;
         if (filterRace.value && filterRace.value.length > 0) {
-          filtered = char.race == filterRace.value
+            filtered = char.race == filterRace.value;
         }
 
         if (filtered) {
-          if (filterClass.value && filterClass.value.length > 0) {
-            filtered = char.className == filterClass.value
-          }
-        }        
+            if (filterClass.value && filterClass.value.length > 0) {
+                filtered = char.className == filterClass.value;
+            }
+        }
 
         return filtered;
     });
 }
 
 function clearSearch() {
-    searchInput.value = "";
+    searchInput.value = '';
     searchActive.value = false;
     filteredChars.value = characters;
     filter();
 }
 
+function clearFilters() {
+    filterRace.value = "";
+    filterClass.value = "";
+    filtersActive.value = false;
+    submit();
+}
+
 function sortDate() {
-    filteredChars.value = filteredChars.value.sort((a,b) => {
+    filteredChars.value = filteredChars.value.sort((a, b) => {
         if (sortOrder.value == 1) {
-            return a.createdAt.localeCompare(b.createdAt)
-        } else  {
-            return -a.createdAt.localeCompare(b.createdAt)
+            return a.createdAt.localeCompare(b.createdAt);
+        } else {
+            return -a.createdAt.localeCompare(b.createdAt);
         }
-    })
+    });
     sortOrder.value = -sortOrder.value;
 }
 </script>
@@ -83,15 +93,40 @@ function sortDate() {
     <div class="char-holder">
         <div class="char-inner">
             <form class="search-form" ref="form" v-on:submit.prevent="submit">
-                <div class="search-form__inner">
-                <label class="search-form__magnifier-label" for="search-input" id="search-label"
-                    ></label>
-                <input class="search-form__input" type="text" v-model="searchInput" aria-label="search term" />           
-                    <button type="reset" title="Nullstill søket" @click="clearSearch" :class="{ hidden: searchInput =='' }" class="search-form__reset clear" aria-label="Nullstill søket">
-                        <svg width="20" height="20" viewBox="0 0 20 20"><path d="M10 10l5.09-5.09L10 10l5.09 5.09L10 10zm0 0L4.91 4.91 10 10l-5.09 5.09L10 10z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                <div class="search-form__input-wrapper">
+                    <input
+                        class="search-form__input"
+                        type="text"
+                        v-model="searchInput"
+                        aria-label="search term"
+                        @keydown.esc="clearSearch"
+                    />
+                    <button
+                        type="reset"
+                        title="Nullstill søket"
+                        @click="clearSearch"
+                        :class="{ hidden: searchInput == '' }"
+                        class="search-form__reset clear"
+                        aria-label="Nullstill søket"
+                    >
+                        <svg width="20" height="20" viewBox="0 0 20 20">
+                            <path
+                                d="M10 10l5.09-5.09L10 10l5.09 5.09L10 10zm0 0L4.91 4.91 10 10l-5.09 5.09L10 10z"
+                                stroke="currentColor"
+                                fill="none"
+                                fill-rule="evenodd"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            ></path>
+                        </svg>
                     </button>
-                    <button class="search-form__magnifier" type="submit" aria-label="search">
-                        <svg width="20" height="20" class="search-form__label" viewBox="0 0 20 20">
+                </div>
+                <button
+                    class="search-form__magnifier search-form__submit"
+                    type="submit"
+                    aria-label="search"
+                >
+                    <svg width="20" height="20" class="search-form__label" viewBox="0 0 20 20">
                         <path
                             d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
                             stroke="currentColor"
@@ -99,29 +134,28 @@ function sortDate() {
                             fill-rule="evenodd"
                             stroke-linecap="round"
                             stroke-linejoin="round"
-                        ></path></svg
-                >
-                    </button>
-            </div>
+                        ></path>
+                    </svg>
+                </button>
             </form>
-            <div>{{ searchActive ? searchInput : "" }}</div>
-            <div class="filters">
-                <select v-model="filterRace" @change="filterChanged()">
-                    <option value="">Velg rase</option>
-                    <option v-for="(item, key) in uniqueRaces" :key="key" :value="item">
-                        {{ item }}
-                    </option>
-                </select>
-                <select v-model="filterClass" @change="filterChanged()">
-                    <option value="">Velg klasse</option>
-                    <option v-for="(item, key) in uniqueClasses" :key="key" :value="item">
-                        {{ item }}
-                    </option>
-                </select>                
-            </div>
+        </div>
+        <div class="filters">
+            <select class="dropdown" v-model="filterRace" @change="filterChanged()">
+                <option value="">Velg rase</option>
+                <option v-for="(item, key) in uniqueRaces" :key="key" :value="item">
+                    {{ item }}
+                </option>
+            </select>
+            <select class="dropdown" v-model="filterClass" @change="filterChanged()">
+                <option value="">Velg klasse</option>
+                <option v-for="(item, key) in uniqueClasses" :key="key" :value="item">
+                    {{ item }}
+                </option>
+            </select>
+            <button v-if="filtersActive" class="clear-button" @click="clearFilters">Nulltill alle</button>
         </div>
         <div class="list">
-            <table class="styled-table">
+            <table v-if="filteredChars.length" class="styled-table">
                 <thead>
                     <tr>
                         <th>Navn</th>
@@ -135,6 +169,11 @@ function sortDate() {
                     <CharItem v-for="(char, i) in filteredChars" :character="char" :key="i" />
                 </tbody>
             </table>
+            <div class="empty" v-else>
+                Ingen resultater.
+                <span v-if="searchActive">Du har søkt på: {{ searchInput }}</span>
+                <span></span>
+            </div>
         </div>
     </div>
 </template>
@@ -142,6 +181,10 @@ function sortDate() {
 <style scoped>
 .filters {
     margin-top: 10px;
+    display: flex;
+    gap: 10px;
+    border-top: 1px solid #ddd;
+    padding: 10px 10px 10px 10px;
 }
 .char-holder {
     margin-top: 30px;
@@ -152,11 +195,25 @@ function sortDate() {
     padding: 10px;
 }
 .list {
-    margin-top: 20px;
+    padding-top: 10px;
     border-top: 1px solid #ddd;
 }
-
 table {
     width: 100%;
+}
+.empty {
+    padding: 20px;
+    display: flex;
+    /* flex-direction: column; */
+}
+.clear-button {
+    outline: none;
+    border: none;
+    background: none;
+    color: #1EA7FD;
+    cursor: pointer;
+}
+.clear-button:hover {
+    text-decoration: underline;
 }
 </style>
